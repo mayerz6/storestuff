e0b2749324032181c4585a49d6e7b537d1a9f297
Larry Mayers <larry.mayers@outlook.com>
2021-01-23 23:50:54 -0400

    Base environment setup for the StoreStuff web application

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+    /* Instantiation of User CLASS to define site visitor engagement */
+    class User {
+
+    }
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>Store Stuff</title>
+            <link rel="text/stylesheet" href="" />    
+    </head>
+
+    <body>
+        <h1>Let's Protect Your Stuff For You!</h1>
+            <section id="">
+                <p>Explore Our Storage Space, Co-Working Space, Co-Warehouse & Services</p>
+                </section>
+
+    </body>
+</html>
